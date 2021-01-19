43675f3e60aa961d93b1170aba1856efaed6b3b1
codegangsk <kjhj0604@gmail.com>
2021-01-19 14:51:32 +0900

    add gitignore

---
+++ YuGiOhCards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YuGiOhCards
+//
+//  Created by Sophie Kim on 2021/01/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
