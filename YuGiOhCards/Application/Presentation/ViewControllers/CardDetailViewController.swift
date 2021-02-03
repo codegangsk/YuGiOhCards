2407d1f654f699d6b8873d4562096f4516d19b80
codegangsk <kjhj0604@gmail.com>
2021-02-03 22:58:16 +0900

    define constraints to image view and labels

--- YuGiOhCards/Application/Presentation/ViewControllers/CardDetailViewController.swift
+++ YuGiOhCards/Application/Presentation/ViewControllers/CardDetailViewController.swift
@@ -15,7 +15,7 @@ class CardDetailViewController: UIViewController {
     var nameLabel = UILabel()
     var atkLabel = UILabel()
     var defLabel = UILabel()
-    var detailLabel = UILabel()
+    var descriptionLabel = UILabel()
 }
 
 extension CardDetailViewController {
@@ -27,6 +27,78 @@ extension CardDetailViewController {
         self.view.addSubview(nameLabel)
         self.view.addSubview(atkLabel)
         self.view.addSubview(defLabel)
-        self.view.addSubview(detailLabel)
+        self.view.addSubview(descriptionLabel)
+        
+        makeImageViewLayout()
+        makeNameLableLayout()
+        makeAtkLabelLayout()
+        makeDefLabelLayout()
+        makeDescriptionLabelLayout()
+        
+        updateUI()
     }
 }
+
+extension CardDetailViewController {
+    func makeImageViewLayout() {
+        self.imageView.addConstraint(NSLayoutConstraint(item: self.imageView,
+                                                        attribute: NSLayoutConstraint.Attribute.height,
+                                                        relatedBy: NSLayoutConstraint.Relation.equal,
+                                                        toItem: self.imageView, attribute: NSLayoutConstraint.Attribute.width,
+                                                        multiplier: 10/17, constant: 0))
+        imageView.contentMode = .scaleToFill
+        imageView.snp.makeConstraints { make in
+            make.top.equalTo(105)
+            make.leading.equalTo(8)
+            make.trailing.equalTo(-8)
+        }
+    }
+    
+    func makeNameLableLayout() {
+        nameLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 18.0)
+        nameLabel.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(8)
+            make.leading.equalTo(8)
+        }
+    }
+    
+    func makeAtkLabelLayout() {
+        atkLabel.snp.makeConstraints { make in
+            make.top.equalTo(nameLabel.snp.bottom).offset(8)
+            make.leading.equalTo(8)
+        }
+    }
+    
+    func makeDefLabelLayout() {
+        defLabel.snp.makeConstraints { make in
+            make.top.equalTo(atkLabel.snp.bottom).offset(8)
+            make.leading.equalTo(8)
+        }
+    }
+    
+    func makeDescriptionLabelLayout() {
+        descriptionLabel.numberOfLines = 20
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(defLabel.snp.bottom).offset(8)
+            make.leading.equalTo(8)
+            make.trailing.equalTo(-8)
+        }
+    }
+}
+
+extension CardDetailViewController {
+    func updateUI() {
+        guard let card = card else { return }
+        nameLabel.text = card.name
+        descriptionLabel.text = card.description
+        
+        if card.atk != nil && card.def != nil {
+            atkLabel.text = "ATK: \(card.atk!)"
+            defLabel.text = "DEF: \(card.def!)"
+        } else {
+            atkLabel.text = "ATK: N/A"
+            defLabel.text = "DEF: N/A"
+        }
+    }
+}
+
