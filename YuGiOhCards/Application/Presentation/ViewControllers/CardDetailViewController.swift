de9dd99b5669bfbd1927f09a7ceb494124a0837e
codegangsk <kjhj0604@gmail.com>
2021-02-03 21:33:24 +0900

    define image view and labels for CardDetailViewController and append to view

--- YuGiOhCards/Application/Presentation/ViewControllers/CardDetailViewController.swift
+++ YuGiOhCards/Application/Presentation/ViewControllers/CardDetailViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import SnapKit
 
 class CardDetailViewController: UIViewController {
-    var card: Card? 
+    var card: Card?
+    
+    var imageView = UIImageView()
+    var nameLabel = UILabel()
+    var atkLabel = UILabel()
+    var defLabel = UILabel()
+    var detailLabel = UILabel()
 }
 
 extension CardDetailViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        self.view.addSubview(imageView)
+        self.view.addSubview(nameLabel)
+        self.view.addSubview(atkLabel)
+        self.view.addSubview(defLabel)
+        self.view.addSubview(detailLabel)
     }
 }
